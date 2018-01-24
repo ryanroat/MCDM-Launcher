647a4b17ce6396bc1ddb1c18fc684073c241c3ec
Ryan Roat <ryanroat@gmail.com>
2018-01-24 15:57:33 -0500

    added changing catchphrases via JS

--- popup.js
+++ popup.js
@@ -1,1 +1,15 @@
-// this extention does not use any JavaScript at this time
+function rando(max) {
+  return (Math.floor(Math.random() * Math.floor(max))+1);   // returns a 'random' number btwn 1 and max inclusive
+}
+
+function phrasePick() {
+  phrases = [
+    'Hey everybody, Matt Colville here.',
+    'You are the DM your group needs.',
+    'The most fun you can have with your brain.'
+  ];
+
+  return phrases [rando(3) - 1];
+}
+
+document.getElementById('phrase').innerHTML = phrasePick ();
