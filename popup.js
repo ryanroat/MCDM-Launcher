e5006945f5366f8effa743a2e888624d78e621b6
Ryan Roat <ryanroat@gmail.com>
2019-10-20 22:03:43 -0400

    prettier'd

--- popup.js
+++ popup.js
@@ -1,16 +1,16 @@
 function rando(max) {
-  return (Math.floor(Math.random() * Math.floor(max))+1);   // returns a 'random' number btwn 1 and max inclusive
+    return Math.floor(Math.random() * Math.floor(max)) + 1; // returns a 'random' number btwn 1 and max inclusive
 }
 
 function phrasePick() {
-  phrases = [
-    'Hey everybody, Matt Colville here.',
-    'You are the DM your group needs.',
-    'D&D is the most fun you can have with your brain.'
-    /* 'D&D is the most fun you<br>can have with your brain.' */
-  ];
+    const phrases = [
+        'Hey everybody, Matt Colville here.',
+        'You are the DM your group needs.',
+        'D&D is the most fun you can have with your brain.'
+        /* 'D&D is the most fun you<br>can have with your brain.' */
+    ];
 
-  return phrases [rando(3) - 1];
+    return phrases[rando(3) - 1];
 }
 
-document.getElementById('phrase').innerHTML = phrasePick ();
+document.getElementById('phrase').innerHTML = phrasePick();
